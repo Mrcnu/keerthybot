5c09dae099bf04239e5b15edc528b40d6393cf23
Mrcnu <134508260+Mrcnu@users.noreply.github.com>
2024-11-14 17:09:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ else:
     LOG_CHANNEL = int(LOG_CHANNEL)
 
 # support group
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '@Naa_Movies_Search')
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1002262310384')
 if len(SUPPORT_GROUP) == 0:
     print('Error - SUPPORT_GROUP is missing, exiting now')
     exit()
